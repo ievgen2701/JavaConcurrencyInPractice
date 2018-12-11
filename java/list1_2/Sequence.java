2857016a688c7fc75b43d5d7de9b621225bfec04
faamorous <ievgen.anikushyn@gmail.com>
2018-12-11 18:33:28 +0200

    thread safe counter

---
+++ java/list1_2/Sequence.java
@@ -0,0 +1,40 @@
+package list1_2;
+
+import annotation.ThreadSafe;
+import net.jcip.annotations.GuardedBy;
+
+@ThreadSafe
+public final class Sequence {
+    
+    @GuardedBy("this") private int value;
+    
+    public synchronized int getNext() {
+        return value++;
+    }
+
+    public static void main(String[] args) {
+        final Sequence sequence = new Sequence();
+
+        final Thread t1 = new Thread(() -> getNextForever(sequence, "Thread1 "));
+        final Thread t2 = new Thread(() -> getNextForever(sequence, "Thread2 "));
+
+        t1.start();
+        t2.start();
+    }
+
+    private static void getNextForever(final Sequence sequence, final String threadPrefix) {
+        while (true) {
+            System.out.println(threadPrefix + sequence.getNext());
+            sleep(1_000);
+        }
+    }
+
+    private static void sleep(final long mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}

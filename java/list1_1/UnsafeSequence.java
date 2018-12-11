86345856fdd79bba1d084ec3ce6b13f4de4ff82c
faamorous <ievgen.anikushyn@gmail.com>
2018-12-11 18:10:33 +0200

    not thread safe counter

---
+++ java/list1_1/UnsafeSequence.java
@@ -0,0 +1,39 @@
+package list1_1;
+
+import annotation.NotThreadSafe;
+
+@NotThreadSafe
+public final class UnsafeSequence {
+
+    private int value;
+
+    public int getNext() {
+        return value++;
+    }
+
+    public static void main(String[] args) {
+        final UnsafeSequence unsafeSequence = new UnsafeSequence();
+
+        final Thread t1 = new Thread(() -> getNextForever(unsafeSequence, "Thread1 "));
+        final Thread t2 = new Thread(() -> getNextForever(unsafeSequence, "Thread2 "));
+
+        t1.start();
+        t2.start();
+    }
+
+    private static void getNextForever(final UnsafeSequence unsafeSequence, final String threadPrefix) {
+        while (true) {
+            System.out.println(threadPrefix + unsafeSequence.getNext());
+            sleep(1_000);
+        }
+    }
+
+    private static void sleep(final long mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
